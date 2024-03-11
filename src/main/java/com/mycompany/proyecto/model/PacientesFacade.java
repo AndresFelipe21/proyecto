004f7da8009884804c3afc937168a45fe1d8b0ec
AndresFelipe21 <111938951+AndresFelipe21@users.noreply.github.com>
2024-03-10 22:17:02 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/proyecto/model/PacientesFacade.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto.model;
+
+import com.mycompany.proyecto.entities.Pacientes;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Andres Felipe
+ */
+@Stateless
+public class PacientesFacade extends AbstractFacade<Pacientes> implements PacientesFacadeLocal {
+
+    @PersistenceContext(unitName = "com.mycompany_Proyecto_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PacientesFacade() {
+        super(Pacientes.class);
+    }
+    
+}
